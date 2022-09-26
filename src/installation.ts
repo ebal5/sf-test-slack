de717b4a1e553529cb33839fb590892fcc288644
ebal5 <6240240+ebal5@users.noreply.github.com>
2022-09-27 02:14:27 +0900

    OAuthリダイレクトのstate検証

--- src/installation.ts
+++ src/installation.ts
@@ -4,10 +4,21 @@ import serverless from 'serverless-http';
 
 const app = express();
 
+const scopes = [
+  "app_mentions:read",
+  "channels:history",
+  "chat:write",
+  "channels:read",
+  "groups:history",
+  "groups:write",
+  "users.profile:read",
+];
+const userScopes = [];
+
 const installer = new InstallProvider({
   clientId: process.env.SLACK_CLIENT_ID!,
   clientSecret: process.env.SLACK_CLIENT_SECRET!,
-  stateVerification: false,
+  authVersion: 'v2',
   stateSecret: "TEST_STATE_SECRET",
 });
 
@@ -17,16 +28,8 @@ app.get('/slack/oauth_redirect', async (req, res) => {
 
 app.get('/slack/install', async (req, res) => {
   await installer.handleInstallPath(req, res, {}, {
-    scopes: [
-      "app_mentions:read",
-      "channels:history",
-      "chat:write",
-      "channels:read",
-      "groups:history",
-      "groups:write",
-      "users.profile:read",
-    ],
-    userScopes: [],
+    scopes,
+    userScopes,
   });
 });
 
