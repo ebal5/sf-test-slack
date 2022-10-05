fcd519513cb63ac149edd08c280de9b1a0e8387e
ebal5 <6240240+ebal5@users.noreply.github.com>
2022-10-06 01:31:17 +0900

    メタデータのテスト

--- src/installation.ts
+++ src/installation.ts
@@ -24,13 +24,35 @@ const installer = new InstallProvider({
 });
 
 app.get('/slack/oauth_redirect', async (req, res) => {
-  await installer.handleCallback(req, res);
+  await installer.handleCallback(req, res, {
+    beforeInstallation: async (options, callbackReq, callbackRes) => {
+      const data = JSON.parse(options.metadata);
+      switch (data.var) {
+        case "test":
+          return true;
+        default:
+          res.end();
+          return false;
+      }
+    },
+  });
 });
 
 app.get('/slack/install', async (req, res) => {
   await installer.handleInstallPath(req, res, {}, {
     scopes,
     userScopes,
+  });
+});
+
+app.get('/:var/slack/install', async (req, res) => {
+  await installer.handleInstallPath(req, res, {
+  }, {
+    scopes,
+    userScopes,
+    metadata: JSON.stringify({
+      var: req.params.var
+    }),
   });
 });
 
